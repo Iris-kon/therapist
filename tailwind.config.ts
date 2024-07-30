fc347e738bc26aa66500b08c47e4c2e8082be4f9
Iris Oliveira <irisoliveira.k.on@gmail.com>
2024-07-30 00:16:15 -0300

    inital config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'primary-blue': '#60a5fa',
+        'secondary-green': '#34d399',
+        'highlight-purple': '#d8b4fe',
+        'neutral-light-gray': '#f3f4f6',
+        'text-dark-gray': '#1f2937',
+        'error-red': '#fc8181',
+        'success-green': '#9ae6b4',
+        'alert-yellow': '#fef08a',
+        'contact-whatsapp': '#25D366',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
